2e5ce6aa2d3f31c619e89122856d1f7165d51697
Nikita Chernyi <rakshazi@openmailbox.org>
2016-01-29 12:20:37 +0200

    Fixed notice for VK video

--- src/Social2Atom/Converter/Vk/Video.php
+++ src/Social2Atom/Converter/Vk/Video.php
@@ -8,7 +8,7 @@ class Video extends \Rakshazi\Social2Atom\Converter\General\Preprocessor
         $this->ready = "Video available only if you set vk.com access token";
         if ($this->di->config('vk.token')) {
             $raw = $this->di->get('vk\API')->videoGet($this->raw->video->owner_id, $this->raw->video->vid);
-            if ($raw->response[0] > 0) {
+            if (isset($raw->response[1]) && $raw->response[1]) {
                 $this->ready = '<iframe allowfullscreen width="607" height="360" src="' . $raw->response[1]->player;
                 $this->ready .= '" frameborder="0"></iframe>';
             }
